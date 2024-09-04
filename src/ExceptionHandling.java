26f11c97956f6018b10b2f6c21c19507c7ffd91d
mayur <kumbharemayur75@gmail.com>
2024-09-04 13:32:01 +0530

    update

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -3,6 +3,7 @@ public class ExceptionHandling {
 	public static void main(String[] args) {
 		try {
 			int result=10/0;
+			
 			System.out.println("Result: "+result);
 		}catch(ArithmeticException e) {
 			System.out.println("Error: Division by zero is not allowed");
--- src/ifelseexample.java
+++ src/ifelseexample.java
@@ -4,6 +4,7 @@ public class ifelseexample {
 		int age = 26;
 		if (age>=18) {
 			System.out.println("adult");
+			//a
 		}
 		else{
 			System.out.println("Not adult");
