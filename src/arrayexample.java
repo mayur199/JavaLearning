67a943eb19e2d6f12b4abb05d234273b6ee22275
mayur <kumbharemayur75@gmail.com>
2024-09-04 13:06:50 +0530

    remove comments

--- src/arrayexample.java
+++ src/arrayexample.java
@@ -5,7 +5,7 @@ public class arrayexample {
 		int[] numbers = {1,2,3,4,5};
 		for(int i=0;i<numbers.length;i++){
 			System.out.println("Element of index "+i+" : "+ numbers[i]);
-			//add comments
+
 		}
 	}
 
