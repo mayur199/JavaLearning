58b25b029e685b1a1baea41835e28c6579ad99fb
mayur <kumbharemayur75@gmail.com>
2025-12-23 11:05:42 +0530

    Ignore logicbasis folder

--- src/com/mayur/studentmarksmanagementsystem/Student.java
+++ src/com/mayur/studentmarksmanagementsystem/Student.java
@@ -3,28 +3,27 @@ package com.mayur.studentmarksmanagementsystem;
 import java.util.ArrayList;
 
 public class Student {
-	private String name;
-	private ArrayList<Integer> marks;
+    private String name;
+    private ArrayList<Integer> marks;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public ArrayList<Integer> getMarks() {
-		return marks;
-	}
+    public ArrayList<Integer> getMarks() {
+        return marks;
+    }
 
-	public void setMarks(ArrayList<Integer> marks) {
-		this.marks = marks;
-	}
+    public void setMarks(ArrayList<Integer> marks) {
+        this.marks = marks;
+    }
 
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", marks=" + marks + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", marks=" + marks + "]";
+    }
 }
